aecdd1a3b84e154077fe6c3009b1ed07dde1fc9c
Adebayo Ayowale G. <haydexwisdom@gmail.com>
2025-10-17 11:11:18 +0100

    SETUPS AND INITIALIZING: Created the boiler plate codes for Projects Startups

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome, Five20TECH  
+        {/* Add your custom welcome message here */}
+        This is the Index screen.
+      </Text>
+    </View>
+  );
+}
