ec13c3529229557f9ef4ba3729558d21e1dffd98
Adebayo Ayowale G. <haydexwisdom@gmail.com>
2025-10-21 17:00:17 +0100

    ALL PENNED FEATURES: Implemented

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,20 @@
 import { Stack } from "expo-router";
-import { ThemeProvider } from "../context/ThemeContest";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider, useThemedColor } from "../context/ThemeContest";
 import { TodoProvider } from "../context/TodoContest";
 import { UserProvider } from "../context/UserContest";
 
 export default function RootLayout() {
+  const AppStatusBar = () => {
+    const { theme } = useThemedColor();
+    return <StatusBar style={theme === "dark" ? "light" : "dark"} />;
+  };
+
   return (
     <ThemeProvider>
       <UserProvider>
         <TodoProvider>
+          <AppStatusBar />
           <Stack screenOptions={{ headerShown: false }}>
             <Stack.Screen name="(tabs)" />
           </Stack>
